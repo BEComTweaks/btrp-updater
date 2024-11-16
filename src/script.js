d64c3e57dd591e6a2a575a2580de9f7d296050fa
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2024-11-16 11:13:12 +0800

    Updated theme + Added pack name changer + supports flattening

--- src/script.js
+++ src/script.js
@@ -1,9 +1,12 @@
 var indexed = {};
-var request = {"name": "<name>", "categories": []};
+var request = { "name": "<name>", "categories": [] };
 const bturl = "https://dev.bedrocktweaks.net/api/resource-packs";
+const zipInput = document.getElementById("zipFileInput");
+var flattened
+
 function logger(msg) {
     console.log(msg);
-    const log = document.querySelector(".log");
+    const log = document.querySelector("pre.log");
     var label = document.createElement("div");
     label.textContent = msg;
     log.appendChild(label);
@@ -15,7 +18,7 @@ fetch(bturl, {
     .then((response) => {
         if (!response.ok) {
             alert(`Something went wrong. ${response.status}`);
-            throw new Error(`HTTP error! status: ${response.status}`);
+            throw new Error(`Status: ${response.status}`);
         }
         return response.json();
     })
@@ -30,97 +33,125 @@ fetch(bturl, {
                 indexed[pack.name] = pack.id;
             })
         })
-        logger("Indexing complete!")
+        logger("Indexing complete!");
+        OreUI.becomeEnabled(document.querySelector(".fileInput"));
+        OreUI.becomeEnabled(zipInput);
     })
     .catch((error) => {
         logger('Error fetching the JSON:', error);
     });
 
-const zipInput = document.getElementById("zipFileInput");
-document.querySelector(".fileInput").addEventListener("click", function() {
+fetch('flattened.json')
+    .then(response => {
+        if (!response.ok) {
+            //how.
+            throw new Error("What have you done")
+        }
+        return response.json();
+    })
+    .then(data => {
+        flattened = data;
+        logger("Received flattened JSON");
+    })
+
+document.querySelector(".fileInput").addEventListener("click", function () {
     zipInput.click()
 })
-zipInput.addEventListener("change", function(event) {
+zipInput.addEventListener("change", function (event) {
     const file = event.target.files[0];
     if (file) {
         logger("File selected");
         logger("Processing file...")
         const reader = new FileReader();
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             JSZip.loadAsync(e.target.result)
                 .then(function (zip) {
+                    let noissue = true;
                     let filesFound = [false, false] // [packs.info, manifest.json]
-                    zip.forEach(function (relativePath, zipEntry) {
-                        if (relativePath.endsWith("packs.info")) {
-                            filesFound[0] = true;
-                            zipEntry.async("string").then(function (content) {
-                                const read = content.replace("Selected Packs:\n", "").replace(/- /g, "").replace(/:/g,"").trim();
-                                const packjson = read.split("\n");
-                                let currentCategory = null;
-                                let invalid = false;
-                                let fuckedUpCombinations = false;
-                                packjson.forEach((e) => {
-                                    if (!fuckedUpCombinations) {
-                                        if (e.startsWith(" ")) { // pack
-                                            e = e.trim();
-                                            if (indexed[e] === undefined) {
-                                                logger(`${e} isn't a valid pack`)
-                                                invalid = true;
-                                            } else {
-                                                currentCategory.packs.push(indexed[e]);
-                                            }
-                                        } else { // category
-                                            if (currentCategory !== null) {
+                    try {
+                        zip.forEach(function (relativePath, zipEntry) {
+                            if (relativePath.endsWith("packs.info")) {
+                                filesFound[0] = true;
+                                zipEntry.async("string").then(function (content) {
+                                    const read = content.replace("Selected Packs:\n", "").replace(/- /g, "").replace(/:/g, "").trim();
+                                    const packjson = read.split("\n");
+                                    let currentCategory = null;
+                                    let invalid = false;
+                                    let fuckedUpCombinations = false;
+                                    packjson.forEach((e) => {
+                                        if (!fuckedUpCombinations) {
+                                            if (e.startsWith(" ")) { // pack
+                                                e = e.trim();
                                                 if (indexed[e] === undefined) {
-                                                    fuckedUpCombinations = true;
+                                                    if (flattened["categories"][currentCategory.id][e] === undefined) {
+                                                        logger(`${e} isn't a valid pack`)
+                                                        invalid = true;
+                                                    } else {
+                                                        currentCategory.packs.push(flattened["categories"][currentCategory.id][e])
+                                                    }
+                                                } else {
+                                                    currentCategory.packs.push(indexed[e]);
                                                 }
-                                                if (currentCategory.packs.length > 0) {
-                                                    request.categories.push(currentCategory);
+                                            } else { // category
+                                                if (currentCategory !== null) {
+                                                    if (indexed[e] === undefined) {
+                                                        fuckedUpCombinations = true;
+                                                    }
+                                                    if (currentCategory.packs.length > 0) {
+                                                        request.categories.push(currentCategory);
+                                                    }
                                                 }
+                                                currentCategory = {
+                                                    id: indexed[e],
+                                                    packs: []
+                                                };
                                             }
-                                            currentCategory = {
-                                                id: indexed[e],
-                                                packs: []
-                                            };
                                         }
+                                    })
+                                    if (invalid) {
+                                        logger("This is probably caused by an internal")
+                                        logger("flattening, and will be ignored")
                                     }
                                 })
-                                if (invalid) {
-                                    logger("This is probably caused by an internal")
-                                    logger("flattening, and will be ignored")
-                                }
-                            })
-                        }
-                        if (relativePath.endsWith("manifest.json")) {
-                            filesFound[1] = true;
-                            zipEntry.async("string").then(function (content) {
-                                try {
-                                    const mf = JSON.parse(content);
-                                    request.name = mf.header.name;
-                                } catch (e) {
-                                    logger("Error parsing manifest.json", e);
-                                }
-                            })
-                        }
-                    })
-                    if (filesFound[0] && filesFound[1]) {
+                            }
+                            if (relativePath.endsWith("manifest.json")) {
+                                filesFound[1] = true;
+                                zipEntry.async("string").then(function (content) {
+                                    try {
+                                        const mf = JSON.parse(content);
+                                        document.querySelector(".packName").value = mf.header.name;
+                                    } catch (e) {
+                                        logger("Error parsing manifest.json", e);
+                                    }
+                                })
+                            }
+                        })
+                    } catch (e) {
+                        logger("Error processing file", e);
+                        noissue = false;
+                    }
+                    if (filesFound[0] && filesFound[1] && noissue) {
                         logger("Processed file");
                         document.querySelector(".fileInput div").innerText = file.name;
-                        document.querySelector(".version").disabled = false;
-                        document.getElementById("processButton").disabled = false;
+                        OreUI.becomeEnabled(document.querySelector(".version"));
+                        OreUI.becomeEnabled(document.getElementById("processButton"))
+                        OreUI.becomeEnabled(document.querySelector(".packName"));
                     } else {
                         logger("Upload a proper pack.")
                     }
                 }
-            )
+                )
         }
         reader.readAsArrayBuffer(file);
     }
 })
 
+var processbutton = document.getElementById("processButton")
 function upload() {
-    logger("Sent POST request");
+    request.name = document.querySelector(".packName").value;
     console.log(request);
+    processbutton.disabled = true;
+    logger("Sent POST request");
     fetch(bturl, {
         method: "POST",
         headers: {
@@ -131,41 +162,45 @@ function upload() {
         .then((response) => {
             if (!response.ok) {
                 alert(`Something went wrong. ${response.status}`);
+                processbutton.disabled = false;
+                throw new Error(`Status: ${response.status}`);
             }
             return response.blob();
         })
         .then(async (blob) => {
             logger("Received pack");
             const reader = new FileReader();
-            reader.onload = function(e) {
+            reader.onload = function (e) {
                 JSZip.loadAsync(e.target.result)
-                .then(function (zip) {
-                    zip.forEach(function (relativePath, zipEntry) {
-                        if (relativePath.endsWith("manifest.json")) {
-                            zipEntry.async("string").then(function (content) {
-                                const mf = JSON.parse(content);
-                                const ver = document.querySelector(".version").value.split(".").map((e) => parseInt(e));
-                                mf.header.min_engine_version = ver;
-                                zip.file("manifest.json", JSON.stringify(mf, null, 2));
-                                logger("Replaced version");
-                                zip.generateAsync({ type: "blob" })
-                                .then(function (updatedBlob) {
-                                    const downloadLink = document.createElement("a");
-                                    downloadLink.href = URL.createObjectURL(updatedBlob);
-                                    downloadLink.download = "updated_pack.zip";
-                                    document.body.appendChild(downloadLink);
-                                    downloadLink.click();
-                                    document.body.removeChild(downloadLink);
-                                    logger("Download pack!")
+                    .then(function (zip) {
+                        zip.forEach(function (relativePath, zipEntry) {
+                            if (relativePath.endsWith("manifest.json")) {
+                                zipEntry.async("string").then(function (content) {
+                                    const mf = JSON.parse(content);
+                                    const ver = document.querySelector(".version").value.split(".").map((e) => parseInt(e));
+                                    mf.header.min_engine_version = ver;
+                                    zip.file("manifest.json", JSON.stringify(mf, null, 2));
+                                    logger("Replaced version");
+                                    zip.generateAsync({ type: "blob" })
+                                        .then(function (updatedBlob) {
+                                            const downloadLink = document.createElement("a");
+                                            downloadLink.href = URL.createObjectURL(updatedBlob);
+                                            downloadLink.download = `${request.name}.zip`;
+                                            document.body.appendChild(downloadLink);
+                                            downloadLink.click();
+                                            document.body.removeChild(downloadLink);
+                                            logger("Download pack!")
+                                        });
                                 });
-                            });
-                        }
+                            }
+                        });
                     });
-                });
             };
             reader.readAsArrayBuffer(blob);
+            processbutton.disabled = false;
         })
         .catch((error) => {
             logger('Error:', response.status);
+            processbutton.disabled = false;
         });
 }
